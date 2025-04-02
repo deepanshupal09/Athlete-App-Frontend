ad683fa63a442c42a63486aff7b116f212fb0289
cannabinoider <shubhamv23030@gmail.com>
2025-04-02 18:58:53 +0530

    upcomingevents added

--- src/app/upcoming/page.tsx
+++ src/app/upcoming/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
+import EventsBox from "@/components/Charts/upcomingevents";
 import { useState } from "react";
+
 
 
 export default function Page() {
   return (
     <>
       <Breadcrumb pageName="Upcoming Events" />
+      <div>
+        <EventsBox/>
+      </div>
 
     </>
   );
